06bac43fc66af3214707dc1ad448d5296ccd2d8d
Andrés Cruz <andrescruz.tech@gmail.com>
2020-02-08 17:32:56 -0300

    Hijacking form control values

--- src/app/cardform/cardform.component.ts
+++ src/app/cardform/cardform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { DateFormControl } from '../date-form-control';
 
 @Component({
   selector: 'app-cardform',
@@ -14,7 +15,7 @@ export class CardformComponent implements OnInit {
       Validators.minLength(16),
       Validators.maxLength(16)
     ]),
-    expiration: new FormControl('', [
+    expiration: new DateFormControl('', [
       Validators.required,
       Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/)
     ]),
