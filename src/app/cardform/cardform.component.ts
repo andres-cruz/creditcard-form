a4b0105a47b474d1fe3a58f9dffc515e11f2f239
Andrés Cruz <andrescruz.tech@gmail.com>
2020-02-08 16:02:35 -0300

    Adding additional inputs

--- src/app/cardform/cardform.component.ts
+++ src/app/cardform/cardform.component.ts
@@ -8,7 +8,10 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class CardformComponent implements OnInit {
   cardForm = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)])
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    cardNumber: new FormControl(''),
+    expiration: new FormControl(''),
+    securityCode: new FormControl('')
   });
 
   constructor() {
