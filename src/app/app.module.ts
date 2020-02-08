c46b48c42f884e7e4dc2141f46a7deb7155a3793
Andrés Cruz <andrescruz.tech@gmail.com>
2020-02-08 10:09:17 -0300

    Creating a form instance

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
+// Components
 import { AppComponent } from './app.component';
+import { CardformComponent } from './cardform/cardform.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CardformComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/cardform/cardform.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-cardform',
+  templateUrl: './cardform.component.html',
+  styleUrls: ['./cardform.component.css']
+})
+export class CardformComponent implements OnInit {
+  cardForm = new FormGroup({
+    name: new FormControl('')
+  });
+
+  constructor() {}
+
+  ngOnInit() {}
+}
